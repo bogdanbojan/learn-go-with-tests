06af25527e82e9a771dbd2b8be7c9115425a8e77
bogdanbojan <bogdanbojan03@gmail.com>
2022-02-02 20:32:00 +0200

    added benchmark and example

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,9 @@
 package iteration
 
-const repeatCount = 5
-
-func Repeat(s string) string {
+// Repeat returns the string rcount times.
+func Repeat(s string, rcount int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < rcount; i++ {
 		repeated += s
 	}
 	return repeated
