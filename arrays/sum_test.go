b4cbac64b56b7f3acabd36ac5987e291c00247f2
bogdanbojan <bogdanbojan03@gmail.com>
2022-02-04 10:10:05 +0200

    updated array tests

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -14,14 +14,14 @@ func TestSum(t *testing.T) {
 		}
 	})
 
-	//t.Run("collection of 5 nr", func(t *testing.T) {
-	//	numbers := [5]int{1, 2, 3, 4, 5}
-	//
-	//	got := Sum(numbers)
-	//	want := 15
-	//
-	//	if got != want {
-	//		t.Errorf("expected %d but got %d, %v", got, want, numbers)
-	//	}
-	//})
+	t.Run("collection of 5 nr", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("expected %d but got %d, %v", got, want, numbers)
+		}
+	})
 }
