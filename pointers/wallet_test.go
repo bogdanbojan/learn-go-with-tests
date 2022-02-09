636ae8ae2443d0b5a25a59eb6bdd33c562e80092
bogdanbojan <bogdanbojan03@gmail.com>
2022-02-09 11:38:12 +0200

    added withdraw negative balance error and refactored the code

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,14 +3,43 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-	want := 10
-
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+	assertError := func(t testing.TB, got error, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	}
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(10)
+		assertBalance(t, wallet, Bitcoin(10))
+
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: 20}
+		wallet.Withdraw(10)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: 10}
+		err := wallet.Withdraw(20)
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, Bitcoin(10))
+
+	})
+
 }
