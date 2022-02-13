97b6d8ec89e222cf778566c52d5f5d7f99fb6d75
bogdanbojan <bogdanbojan03@gmail.com>
2022-02-13 18:04:23 +0200

    added the app folder. updated reflection

---
+++ app/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type PlayerStore interface {
+	GetPlayerStore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprintf(w, GetPlayerScore(player)) // make it a method
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+
+	return ""
+}
---
+++ app/server_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("return Pepper's score", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "20")
+	})
+
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return request
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
